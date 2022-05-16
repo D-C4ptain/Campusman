e3493e97a02153e33e59e543fd5f6c24dc1d2596
Morvin-Ian <morvinian@gmail.com>
2022-05-16 16:12:22 +0300

    portal

--- campusman/urls.py
+++ campusman/urls.py
@@ -20,7 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('baseapp.urls'))
+    path('', include('baseapp.urls')),
+    path('portal/', include('portal.urls'))
 ]
 
 if settings.DEBUG:
