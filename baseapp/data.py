f721ae7d4354a986399f1488f59271eeea3e2ae6
Morvin-Ian <morvinian@gmail.com>
2022-10-15 17:51:10 +0300

    course additions

--- baseapp/data.py
+++ baseapp/data.py
@@ -1,14 +1,13 @@
-Faculty_choices = (
+programs = (
     ("1","School of Space Science"),
     ("2","School of Engineering"),
     ("3","School of Art"),
     ("4","School of Infocoms"),
     ("5","School of Business"),
-    ("6","School of Aeronautical Engineering"),
-    ("7","School of Medicine"),
+    ("6","School of Medicine"),
 ) 
 
-Actual_programs = [
+courses = [
 
             ("School of Space Science",
              (
@@ -44,26 +43,34 @@ Actual_programs = [
                 (6, "BSc in Automobile Engineering"),
                 (7, "Bsc Biomedical engineering"),
              )
+            ),
+
+            ("School of Art", 
+            (
+                (1, "Bachelor of Arts (Kiswahili)"),
+                (2, "Bachelor of Arts (English)"),
+                (3, "Bachelor of Arts (German)"),
+                (4, "Bachelor of Arts (French)"),
             )
+            ),
 
+            ("School of Business", 
+            (
+                (1, "Bachelor of Accounting and Finance"),
+                (2, "Bachelor of Business Administration and Entreprenuership"),
+                (3, "Bachelor of Management Science and Technology)"),
+                (4, "Bachelor of Economics And Resource Management"),
+            )
+            ),
 
-     
-
+            ("School of Medicine", 
+            (
+                (1, "Department of Human Anatomy"),
+                (2, "Bachelors of Pathology"),
+                (3, "Bachelor of Medical Microbiology & Parasitology.)"),
+                (4, "Bachelor of Paediatrics and Child Health"),
+                (5, "Bachelors of Psychiatry and Mental health.")
+            )
+            ),
 ]
-
-
-    # "School of Art":[
-
-    # ],
-
-
-
-    # "School of Infocoms":[
-
-    # ],
-
-    # "School of Medicine":[],
-
-    # "School of Aeronautical Engineering":[],
-
 
