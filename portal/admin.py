c2f46f99d30cf9d3b3367f952a1d8e5aec917115
Morvin-Ian <morvinian@gmail.com>
2022-10-15 19:11:12 +0300

    course additions

--- portal/admin.py
+++ portal/admin.py
@@ -2,4 +2,5 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(StudentDetails)
-admin.site.register(SchoolPrograms)
+admin.site.register(ICTDetails)
+admin.site.register(FinanceDetails)
--- portal/views.py
+++ portal/views.py
@@ -1,2 +1,12 @@
+from tkinter import S
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, TemplateView
+from .models import ICTDetails, FinanceDetails, StudentDetails
 
+
+class StudentLogin(TemplateView):
+    model = StudentDetails
+    
+class StudentIndexView(DetailView):
+    model = StudentDetails
+  
