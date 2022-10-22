e2f648f1e2ee7f461b148e7453b82bebba8c0c28
Morvin-Ian <morvinian@gmail.com>
2022-10-22 17:40:50 +0300

    courses

--- portal/urls.py
+++ portal/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('student/<int:pk>/', StudentIndexView.as_view(template_name='portal/detail.html'), name='student-portal')
+    path('student/<int:pk>/', StudentIndexView.as_view(template_name='portal/studentdetail.html'), name='student-portal')
    
 ]
