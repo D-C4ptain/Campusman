2872cc665797dd5bb2602b6f4ae511eba5abcdf3
d-captainkenya <dcaptainkenya@gmail.com>
2022-10-22 17:41:52 +0300

    gss

--- admission/urls.py
+++ admission/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import AdmissionView
+from .views import AdmissionView, AdmissionGssView, AdmissionPssView
 
 urlpatterns = [
-    path('', AdmissionView.as_view(), name='admission')
+    path('', AdmissionView.as_view(), name='admission'),
+    path('gss/', AdmissionGssView.as_view(), name='gss'),
+    path('pss/', AdmissionPssView.as_view(), name='pss'),
 ]
--- admission/views.py
+++ admission/views.py
@@ -5,3 +5,9 @@ from django.views.generic import TemplateView
 
 class AdmissionView(TemplateView):
     template_name = 'admission/admission_procedure.html'
+    
+class AdmissionGssView(TemplateView):
+    template_name = 'admission/admission_gss.html'
+    
+class AdmissionPssView(TemplateView):
+    template_name = 'admission/admission_pss.html'
